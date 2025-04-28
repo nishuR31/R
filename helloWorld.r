096fe58631c98d8df78c5cfd6266015941c1d315
Nishu <150690474+nishuR31@users.noreply.github.com>
2025-04-29 00:16:35 +0530

    Create helloWorld.r

---
+++ helloWorld.r
@@ -0,0 +1,15 @@
+# Basic Hello World
+print("Hello World!")
+
+# Alternative version (works in interactive mode)
+#"Hello World!"
+
+# Using variables
+hello_string <- "Hello World!"
+print(hello_string)
+
+# Function version
+hello_world <- function() {
+  print("Hello World!")
+}
+hello_world()
